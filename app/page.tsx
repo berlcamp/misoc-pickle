1cc68a9c2c5f3c0fe91f49e73ff41cce8bf08f0e
Berl treasure Campomanes <berlcamp@gmail.com>
2025-10-10 18:27:39 +0800

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,11 @@ interface FormTypes {
 }
 
 const categories = [
-  {
-    name: 'novice',
-    label: 'Novice',
-    link: 'category/novice'
-  },
+  // {
+  //   name: 'novice',
+  //   label: 'Novice',
+  //   link: 'category/novice'
+  // },
   {
     name: 'bm',
     label: 'Beginner Men',
